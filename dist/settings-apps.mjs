350666ec375baff19c1c05d4428084ccc792fb1f
Ferdinand Thiessen <opensource@fthiessen.de>
2024-05-02 23:41:24 +0200

    chore: Compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ dist/settings-apps.mjs
@@ -0,0 +1,2 @@
+/*! third party licenses: dist/vendor.LICENSE.txt */
+import{r as i}from"./chunks/rebuild-navigation-C74KjqNn.mjs";import"./core-common.mjs";window.OC.Settings=window.OC.Settings||{},window.OC.Settings.Apps=window.OC.Settings.Apps||{rebuildNavigation:i};
