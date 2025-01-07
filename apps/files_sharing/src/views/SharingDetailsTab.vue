c43bbd2c14406695bf76f11ed69cf3727d4fb8e5
nfebe <fenn25.fn@gmail.com>
2025-01-07 11:14:43 +0100

    feat(files_sharing): add send mail toggle
    For new internal shares, show the user a toggle for them configure if they want
    to send the share receiver a notification mail.
    
    This is subject to wether the configured user has an email send up
    
    Signed-off-by: nfebe <fenn25.fn@gmail.com>

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -84,6 +84,12 @@
 					</NcCheckboxRadioSwitch>
 				</div>
 			</div>
+			<div v-if="isNewShare && isUserShare" class="sharingTabDetailsView__general-control">
+				<NcCheckboxRadioSwitch type="switch"
+					:checked.sync="sendNotificationMailOnCreation">
+					{{ t('settings', 'Send email notification') }}
+				</NcCheckboxRadioSwitch>
+			</div>
 			<div class="sharingTabDetailsView__advanced-control">
 				<NcButton id="advancedSectionAccordionAdvancedControl"
 					type="tertiary"
@@ -329,8 +335,8 @@ export default {
 	data() {
 		return {
 			writeNoteToRecipientIsChecked: false,
+			sendNotificationMailOnCreation: true,
 			sharingPermission: BUNDLED_PERMISSIONS.ALL.toString(),
-			revertSharingPermission: BUNDLED_PERMISSIONS.ALL.toString(),
 			setCustomPermissions: false,
 			passwordError: false,
 			advancedSectionAccordionExpanded: false,
@@ -923,6 +929,10 @@ export default {
 					incomingShare.password = this.share.password
 				}
 
+				if (this.sendNotificationMailOnCreation) {
+					incomingShare.sendMail = 'true'
+				}
+
 				this.creating = true
 				const share = await this.addShare(incomingShare)
 				this.creating = false
@@ -965,6 +975,7 @@ export default {
 					attributes: JSON.stringify(share.attributes),
 					...(share.note ? { note: share.note } : {}),
 					...(share.password ? { password: share.password } : {}),
+					...(share.sendMail ? { sendMail: share.sendMail } : {}),
 				})
 				return resultingShare
 			} catch (error) {
@@ -1125,13 +1136,9 @@ export default {
 		}
 	}
 
-	&__advanced-control {
+	&__advanced-control, &__general-control {
 		width: 100%;
-
-		button {
-			margin-top: 0.5em;
-		}
-
+		margin-top: 0.5em;
 	}
 
 	&__advanced {
