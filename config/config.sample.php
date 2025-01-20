ibpl/server

commit f58ce7e7b044ca8b327f2a30a13c770fdb632d7e

Added parameter to disable web UI integration with local client
Added system config parameter `web_ui_integration_local_client.enabled`
to allow disabling integration with local client from web UI (i.e. when
local Nextcloud clients are not used).

Use
```
occ config:system:set web_ui_integration_local_client.enabled --value='true' --type=boolean
```
to enable (default if not set) and
```
occ config:system:set web_ui_integration_local_client.enabled --value='false' --type=boolean
```
to disable  integration.

Use added parameter to show/hide `Edit locally` option in Nextcloud Files.

Related: https://help.nextcloud.com/t/how-remove-hide-edit-locally-option-in-web-interface/171999/5
Author-Change-Id: IB#1156402
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

config/config.sample.php
@@ -2606,4 +2606,11 @@ $CONFIG = [
  * Defaults to 5.
  */
 'files.chunked_upload.max_parallel_count' => 5,
+
+/**
+ * Enables web UI integration with local client.
+ *
+ * Defaults to ``true``
+ */
+'web_ui_integration_local_client.enabled' => true,
 ];

apps/files/lib/Controller/ViewController.php
@@ -3,6 +3,7 @@
 /**
  * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
+ * SPDX-FileCopyrightText: 2025 Informatyka Boguslawski sp. z o.o. sp.k.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 namespace OCA\Files\Controller;
@@ -196,6 +197,7 @@ class ViewController extends Controller {
 
 		$this->initialState->provideInitialState('templates_path', $this->templateManager->hasTemplateDirectory() ? $this->templateManager->getTemplatePath() : false);
 		$this->initialState->provideInitialState('templates', $this->templateManager->listCreators());
+		$this->initialState->provideInitialState('integration_local_client_enabled', ($this->config->getSystemValueBool('web_ui_integration_local_client.enabled', true)));
 
 		$response = new TemplateResponse(
 			Application::APP_ID,

apps/files/src/actions/editLocallyAction.ts
@@ -1,5 +1,6 @@
 /**
  * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2025 Informatyka Boguslawski sp. z o.o. sp.k.
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 import { encodePath } from '@nextcloud/paths'
@@ -12,6 +13,9 @@ import axios from '@nextcloud/axios'
 import LaptopSvg from '@mdi/svg/svg/laptop.svg?raw'
 import IconWeb from '@mdi/svg/svg/web.svg?raw'
 import { isPublicShare } from '@nextcloud/sharing/public'
+import { loadState } from '@nextcloud/initial-state'
+
+let integrationLocalClientEnabled = loadState<boolean>('files', 'integration_local_client_enabled', true)
 
 const confirmLocalEditDialog = (
 	localEditCallback: (openingLocally: boolean) => void = () => {},
@@ -85,6 +89,12 @@ export const action = new FileAction({
 
 	// Only works on single files
 	enabled(nodes: Node[]) {
+
+		// Only works when enabled.
+		if (!integrationLocalClientEnabled) {
+			return false
+		}
+
 		// Only works on single node
 		if (nodes.length !== 1) {
 			return false

commit 27f49d65d564e5c0af89557edf654606ee05f92b

Add missing copyright
Fixes: f58ce7e7b044ca8b327f2a30a13c770fdb632d7e
Author-Change-Id: IB#1156402
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

config/config.sample.php
@@ -3,6 +3,7 @@
 /**
  * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
+ * SPDX-FileCopyrightText: 2025 Informatyka Boguslawski sp. z o.o. sp.k.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 