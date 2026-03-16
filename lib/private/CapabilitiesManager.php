89aa1ed537b0e033888dfe65d725ea64ca1d612d
Simon L. <szaimen@e.mail.de>
2026-03-16 11:34:08 +0100

    fix(CapabilitiesManager): only check execution time if debug mode is enabled
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- lib/private/CapabilitiesManager.php
+++ lib/private/CapabilitiesManager.php
@@ -12,6 +12,7 @@ use OCP\AppFramework\QueryException;
 use OCP\Capabilities\ICapability;
 use OCP\Capabilities\IInitialStateExcludedCapability;
 use OCP\Capabilities\IPublicCapability;
+use OCP\IConfig;
 use OCP\ILogger;
 use Psr\Log\LoggerInterface;
 
@@ -56,26 +57,32 @@ class CapabilitiesManager {
 						// that we would otherwise inject to every page load
 						continue;
 					}
+
 					$startTime = microtime(true);
 					$capabilities = array_replace_recursive($capabilities, $c->getCapabilities());
 					$endTime = microtime(true);
-					$timeSpent = $endTime - $startTime;
-					if ($timeSpent > self::ACCEPTABLE_LOADING_TIME) {
-						$logLevel = match (true) {
-							$timeSpent > self::ACCEPTABLE_LOADING_TIME * 16 => ILogger::FATAL,
-							$timeSpent > self::ACCEPTABLE_LOADING_TIME * 8 => ILogger::ERROR,
-							$timeSpent > self::ACCEPTABLE_LOADING_TIME * 4 => ILogger::WARN,
-							$timeSpent > self::ACCEPTABLE_LOADING_TIME * 2 => ILogger::INFO,
-							default => ILogger::DEBUG,
-						};
-						$this->logger->log(
-							$logLevel,
-							'Capabilities of {className} took {duration} seconds to generate.',
-							[
-								'className' => get_class($c),
-								'duration' => round($timeSpent, 2),
-							]
-						);
+
+					// Only check execution time if debug mode is enabled
+					$debugMode = \OCP\Server::get(IConfig::class)->getSystemValueBool('debug', false);
+					if ($debugMode) {
+						$timeSpent = $endTime - $startTime;
+						if ($timeSpent > self::ACCEPTABLE_LOADING_TIME) {
+							$logLevel = match (true) {
+								$timeSpent > self::ACCEPTABLE_LOADING_TIME * 16 => ILogger::FATAL,
+								$timeSpent > self::ACCEPTABLE_LOADING_TIME * 8 => ILogger::ERROR,
+								$timeSpent > self::ACCEPTABLE_LOADING_TIME * 4 => ILogger::WARN,
+								$timeSpent > self::ACCEPTABLE_LOADING_TIME * 2 => ILogger::INFO,
+								default => ILogger::DEBUG,
+							};
+							$this->logger->log(
+								$logLevel,
+								'Capabilities of {className} took {duration} seconds to generate.',
+								[
+									'className' => get_class($c),
+									'duration' => round($timeSpent, 2),
+								]
+							);
+						}
 					}
 				}
 			} else {
